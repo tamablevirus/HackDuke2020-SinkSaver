ed6d89d87a205d5f0ca0550fdbf2f0d00d834c05
Amin Zamani <abz@email.unc.edu>
2020-12-05 18:36:42 -0500

    Set up basic Flask environment #2

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import csv
+import os
 
 app = Flask(__name__)
+app.secret_key = "asdfasfdasfdsafasddfsadfasdfsadfdas"
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+base = os.getcwd()
 
 @app.route('/')
 def hello_world():
