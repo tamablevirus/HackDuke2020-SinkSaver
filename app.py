799adb43175668a83aff5a54cd9bcd4bef13e1c3
Amin Zamani <abz@email.unc.edu>
2020-12-05 20:16:01 -0500

    Update image fetching

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.secret_key = "asdfasfdasfdsafasddfsadfasdfsadfdas"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 base = os.getcwd()
 
+client = vision.ImageAnnotatorClient.from_service_account_file(
+    os.path.normpath(os.path.normpath(base + '/sinksaver-82c12b4843e4.json')))
 
 @app.route('/')
 def hello_world():
@@ -21,24 +23,22 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route("/image", methods=["GET"])
-def getImage():
-    return send_file("test.jpg", mimetype="image")
-@app.route("/upload", methods=["POST"])
+@app.route("/image/<name>", methods=["GET"])
+def getImage(name):
+    return send_file(name, mimetype="image")
+@app.route("/upload/<content>", methods=["POST"])
 def upload():
     image_helper_google(request.files['image'])
 
 def image_helper_google(image):
-    client = vision.ImageAnnotatorClient.from_service_account_file(
-        os.path.normpath(os.path.normpath(base + '/sinksaver-82c12b4843e4.json')))
     req = {
-        "image": {"source": {'filename': image}},
+        "image": {"source": {'filename': ''}},
         "features": [
             {"type": vision.Feature.Type.LABEL_DETECTION},
         ]
     }
     resp = client.annotate_image({
-  'image': {'source': {'image_uri': image}},
+  'image': {'source': {'image_uri': 'https://sink-saver.herokuapp.com/image/'+image}},
   'features': [{'type_': vision.Feature.Type.FACE_DETECTION}]
 })
 
@@ -46,5 +46,5 @@ def image_helper_google(image):
     print(resp)
 
 if __name__ == '__main__':
-    image_helper_google(os.path.normpath(base+'/amin_sink_running.png'))
+    image_helper_google('test.jpg')
     app.run()
