f484221f92f81acc2ef369f53fea39d886c1c326
TamableVirus <ka.bir.kalra1@gmail.com>
2020-12-05 19:51:53 -0500

    git ignore stuff

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import csv
 import os
-
+import sys 
+from google.cloud import vision
 app = Flask(__name__)
 app.secret_key = "asdfasfdasfdsafasddfsadfasdfsadfdas"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -10,6 +11,11 @@ base = os.getcwd()
 
 @app.route('/')
 def hello_world():
+    client = vision.ImageAnnotatorClient()
+    response = client.annotate_image({
+        'image' : {'source' : {'image_uri' : "./test.jpg"}}
+    })
+    print(response, flush=True)
     return 'Hello World!'
 
 
