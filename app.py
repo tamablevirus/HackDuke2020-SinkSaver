a2b50141661317a7e52c222fb6ca88cd3f95d1b6
Amin Zamani <abz@email.unc.edu>
2020-12-06 00:37:38 -0500

    Google Service Key Fix #4

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ base = os.getcwd()
 app.config['UPLOAD_FOLDER'] = base +'/static'
 
 
-client = vision.ImageAnnotatorClient.from_service_account_json(os.environ['GOOGLE_SERVICE_ACCOUNT'])
+client = vision.ImageAnnotatorClient.from_service_account_file(base+'/key.json')
 twilio_client = Client(os.environ['TWILIO_SID'],os.environ['TWILIO_AUTH'])
 
 last_time_water_running = -1
